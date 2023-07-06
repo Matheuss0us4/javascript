037686ef89e901d5a30d8c0ce0f2c0f737694eb2
Matheus sousa <123515705+Matheuss0us4@users.noreply.github.com>
2023-07-06 19:32:48 -0300

    criando aula 04: ex 8, 9, 10

--- aula-04/ex008.js
+++ aula-04/ex008.js
@@ -1,1 +1,6 @@
-console.log('O console funcionou normalmente!') 
+ var vel = 40
+console.log(`A velocidade do carro e de ${vel} km/h`)
+if (vel > 60) {//condiçao simples, porque ela nao tem um se nao (else) so tera uma condiçao s passar acima do numero definido na varilvel dentro de (if )
+    console.log('Voce utrapassou a velocidade permitida. MULTADO!')
+}
+console.log('Dirija sempre usando sinto de segurança')
---
+++ aula-04/ex009.js
@@ -0,0 +1,8 @@
+var pais = "canada"
+console.log(`Vivendo em ${pais}`)
+if (pais == 'Brasileiro') {
+    console.log('Voce e Canadese')
+} else {
+    console.log('Voce estrangeiro')
+}
+
