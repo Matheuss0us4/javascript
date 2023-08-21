d393285886015bc860df7cf63e036b9575863b74
Matheus sousa <123515705+Matheuss0us4@users.noreply.github.com>
2023-08-21 08:55:25 -0300

    criando ex011

--- aula-05/ex011.js
+++ aula-05/ex011.js
@@ -1,4 +1,25 @@
-var idade = 21
+var nota = 101
+console.log(`Voce tirou a nota ${nota}`)
+if(nota > 600) {
+    console.log('Parabens voçe passou!')
+} else if (nota > 100 || nota < 70) {
+    console.log('Recuperaçao!')
+} else {
+    console.log('Reprovado!')
+}
+
+
+
+
+
+
+
+
+
+
+
+
+/* var idade = 21
 console.log(`Voçe tem ${idade} anos`)
 if (idade < 16) {
     console.log('Nao vota!')
@@ -7,3 +28,4 @@ if (idade < 16) {
 } else {
     console.log('Voto Obrigatorio!')
 }
+ */
