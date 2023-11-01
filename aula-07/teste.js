89891231eb4d5c6634687a14b669a22bd46c5505
Matheus sousa <123515705+Matheuss0us4@users.noreply.github.com>
2023-11-01 16:15:01 -0300

    criando aula-07

---
+++ aula-07/teste.js
@@ -0,0 +1,19 @@
+var c = 1
+do {
+    console.log(`passo ${c}`)
+    c++
+} while(c <= 5)
+
+
+/* var c = 1
+while(c <= 5) {
+    console.log(`passo ${c}`)
+    c++
+} */
+
+
+/* console.log('Tudo bem')
+console.log('Tudo bem')
+console.log('Tudo bem')
+console.log('Tudo bem')
+console.log('Tudo bem') */
