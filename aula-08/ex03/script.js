0f4245bd8e2642e86d6f2506cb3e6e115a020c96
Matheus sousa <123515705+Matheuss0us4@users.noreply.github.com>
2023-12-05 15:25:10 -0300

    atualizando aula-08 ex03

--- aula-08/ex03/script.js
+++ aula-08/ex03/script.js
@@ -1,39 +1,19 @@
-/* function contar() {
-    var resu = 0
-    while(resu <= 5) {
-        document.getElementById('res').innerHTML  +=resu
-        resu++
-    }
-} */
-
 function contar() {
-    var n1 = document.getElementById('txn1')
-    var n2 = document.getElementById('txn2')
-    var n3 = document.getElementById('txn3')
-    var resu = document.getElementById('res')
-    var ini = Number(n1.value)
-    var fim = Number(n2.value)
-    var salto = Number(n3.value) 
-    if(ini == 0 && fim == 0) {
-        window.alert('Insira o inicio e fim')
-    }  else if (salto == 0) {
-        window.alert('[ERRO!] salto igual 0')
+    let ini = document.getElementById('txt1')
+    let fim = document.getElementById('txt2')
+    let passo = document.getElementById('txt3')
+    let res = document.getElementById('res')
+
+    if(ini.value.length == 0  || fim.value.length == 0  || passo.value.length == 0) {
+        window.alert('ERRO! faltam dados.')
     } else {
-        if (ini < fim) {
-            resu.innerHTML = 'CONTAGEM PROGRESSIVA<br>'
-            while(ini <= fim){
-                resu.innerHTML +=ini
-                ini = ini + salto
-            }
-        } else {
-            resu.innerHTML = 'CONTAGEM REGRESSIVA<br>'
-            while(ini >= fim) {
-                JSON(emo)
-                resu.innerHTML +=ini
-                ini = ini - salto
-            }
+        res.innerHTML = 'Contando...'
+        let i = Number(ini.value)
+        let f = Number(fim.value)
+        let p = Number(passo.value)
+
+        for(let c = i;c <= f; c+= p) {
+             res.innerHTML += `${c} {1F603}`  
         }
     }
-
-    
 }
