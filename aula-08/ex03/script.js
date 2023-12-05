083d5cd64f35936d391f83003bfde97048e90d38
Matheus sousa <123515705+Matheuss0us4@users.noreply.github.com>
2023-12-05 16:23:48 -0300

    atualizando aula-08 ex03

--- aula-08/ex03/script.js
+++ aula-08/ex03/script.js
@@ -5,15 +5,28 @@ function contar() {
     let res = document.getElementById('res')
 
     if(ini.value.length == 0  || fim.value.length == 0  || passo.value.length == 0) {
-        window.alert('ERRO! faltam dados.')
+        res.innerHTML = 'Imposível contar!'
     } else {
-        res.innerHTML = 'Contando...'
+        res.innerHTML = 'Contando...<br>'
         let i = Number(ini.value)
         let f = Number(fim.value)
         let p = Number(passo.value)
-
-        for(let c = i;c <= f; c+= p) {
-             res.innerHTML += `${c} {1F603}`  
+        if(p <= 0) {
+            window.alert('Passo invalido! Considerando PASSO 1')
+            p = 1
         }
-    }
+
+        if(i < f) {
+            //contagem progressiva
+            for(let c = i;c <= f; c+= p) {
+                res.innerHTML += ` ${c} \u{1F449}`  
+           }   
+        } else {
+            //contagem regressiva
+            for(let c = i;c >= f; c -= p) {
+                res.innerHTML += `${c} \u{1F449}`
+            }  
+            }res.innerHTML += '\u{1F3C1}'
+        }
+    
 }
