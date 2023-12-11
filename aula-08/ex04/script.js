3c8d11a8b312a81e77a8214001a71f8aee6ee4a0
Matheus sousa <123515705+Matheuss0us4@users.noreply.github.com>
2023-12-11 19:31:28 -0300

    atualizando aula-08 ex04

--- aula-08/ex04/script.js
+++ aula-08/ex04/script.js
@@ -1,8 +1,24 @@
 function tabuada() {
+    /* criar as variaveis */
     let res = document.getElementById('res')
-    let c = 0
-    while(c <= 3) {
-        res.innerHTML += `2 x ${c}<br> `
+    let tabu = document.getElementById('tabu')
+    if(tabu.value.length == 0) {
+        window.alert('insira um numero!')
+    } else {
+        let t = Number(tabu.value)
+        let r = 1
+        for(let t = tab;tab <= 10; tab++) {
+            res.innerHTML += tab
+        }
+    } 
+    
+    /* criar uma condiçao na qual se o input do numero da tabuada for 0 ou nada entao 'INSERIR UM NUMERO!'*/
+
+    /* proseguir a condiçao com o else, converter o inpute tabuada para numero */
+    
+    while(c <= 10) {
+        r = c * t
+        res.innerHTML += `${c} x ${t} = ${r}<br>`
         c++
     }
 }
