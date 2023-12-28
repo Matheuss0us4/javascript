fe4dc9dc597b81604c0366a5f96b00a36572e463
Matheus sousa <123515705+Matheuss0us4@users.noreply.github.com>
2023-12-28 18:12:12 -0300

    criando aula-09

--- aula-09/ambiente.js
+++ aula-09/ambiente.js
@@ -1,4 +1,6 @@
-let num = [5, 3, 7, 12, 2]
-num.push(9)
-num.length
-console.log(`Nosso vetor é o ${num}`) 
+let num = [5, 3, 7, 9, 2]
+num.push(1)
+num.sort()
+console.log(num)
+console.log(`O vetor tem ${num.length} posições`) 
+console.log(`O primero numero do vetor é ${num[0]}`)
