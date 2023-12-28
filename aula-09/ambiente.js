e61d6342569bced7b5c3e4cae162b29391f5866b
Matheus sousa <123515705+Matheuss0us4@users.noreply.github.com>
2023-12-28 19:24:56 -0300

    atualizando aula-9

--- aula-09/ambiente.js
+++ aula-09/ambiente.js
@@ -1,6 +1,7 @@
 let num = [5, 3, 7, 9, 2]
 num.push(1)
-num.sort()
+//num.sort()
 console.log(num)
 console.log(`O vetor tem ${num.length} posições`) 
 console.log(`O primero numero do vetor é ${num[0]}`)
+console.log(`o valor 7 esta na posição `,num.indexOf(7))
